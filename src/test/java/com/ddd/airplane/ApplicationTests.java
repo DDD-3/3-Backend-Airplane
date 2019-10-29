80773e98e7b4d2a90ab6810de925ac79cb496ca8
윤준석 <junseok.youn94@navercorp.com>
2019-10-29 21:44:46 +0900

    프로젝트 생성
    - 스프링 이니셜라이저로 프로젝트 생성
    - 의존성 추가 : Web, JPA, H2, Lombok
    - 참고 : https://spring.io/guides/tutorials/rest/

---
+++ src/test/java/com/ddd/airplane/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddd.airplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
