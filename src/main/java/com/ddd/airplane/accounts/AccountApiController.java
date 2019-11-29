b82b962562ac0eab15e1e557837248b9362135e4
윤준석 <y2o2u2n@gmail.com>
2019-11-30 01:10:38 +0900

    사용하지 않는 import 제거

--- src/main/java/com/ddd/airplane/accounts/AccountApiController.java
+++ src/main/java/com/ddd/airplane/accounts/AccountApiController.java
@@ -2,10 +2,6 @@ package com.ddd.airplane.accounts;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
