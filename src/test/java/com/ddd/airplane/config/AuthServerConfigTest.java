e3dded55f14f85887817330233ff47d1579aa79e
윤준석 <y2o2u2n@gmail.com>
2019-11-29 22:33:58 +0900

    Refresh 토큰 테스트 추가

--- src/test/java/com/ddd/airplane/config/AuthServerConfigTest.java
+++ src/test/java/com/ddd/airplane/config/AuthServerConfigTest.java
@@ -7,6 +7,8 @@ import com.ddd.airplane.common.BaseControllerTest;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.util.Jackson2JsonParser;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -50,5 +52,47 @@ public class AuthServerConfigTest extends BaseControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("access_token").exists());
+    }
+
+    @Test
+    public void getAuthToken_using_refreshToken() throws Exception {
+        String refreshToken = getRefreshToken();
+
+        mockMvc.perform(
+                post("/oauth/token")
+                        .with(httpBasic(appProperties.getClientId(), appProperties.getClientSecret()))
+                        .param("grant_type", "refresh_token")
+                        .param("refresh_token", refreshToken)
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("access_token").exists())
+                .andExpect(jsonPath("refresh_token").value(refreshToken));
+    }
+
+    private String getRefreshToken() throws Exception {
+        String email = "sample@gmail.com";
+        String password = "password";
+        String nickname = "sample";
+
+        AccountDto accountDto = AccountDto.builder()
+                .email(email)
+                .password(password)
+                .nickname(nickname)
+                .build();
+
+        accountService.createAccount(accountDto);
+
+        ResultActions perform = mockMvc.perform(
+                post("/oauth/token")
+                        .with(httpBasic(appProperties.getClientId(), appProperties.getClientSecret()))
+                        .param("username", email)
+                        .param("password", password)
+                        .param("grant_type", "password")
+        );
+
+        String responseBody = perform.andReturn().getResponse().getContentAsString();
+        Jackson2JsonParser parser = new Jackson2JsonParser();
+        return parser.parseMap(responseBody).get("refresh_token").toString();
     }
 }
