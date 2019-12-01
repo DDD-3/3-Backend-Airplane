ba067acca85ca83e913138adcde5d21067738639
윤준석 <y2o2u2n@gmail.com>
2019-12-02 00:20:46 +0900

    Getting Started 웹소켓

---
+++ src/main/java/com/ddd/airplane/chats/Greeting.java
@@ -0,0 +1,12 @@
+package com.ddd.airplane.chats;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Greeting {
+    private String content;
+}
