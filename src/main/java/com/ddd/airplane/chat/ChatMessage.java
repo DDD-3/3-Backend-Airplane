9178e7a265e097a11af39245a18fda826edb975a
윤준석 <y2o2u2n@gmail.com>
2019-12-10 02:04:44 +0900

    Update ChatMessage.java

--- src/main/java/com/ddd/airplane/chat/ChatMessage.java
+++ src/main/java/com/ddd/airplane/chat/ChatMessage.java
@@ -3,13 +3,10 @@ package com.ddd.airplane.chat;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
-
 @Getter @Setter
 class ChatMessage {
     private ChatMessageType type;
     private Long roomId;
     private String senderId;
     private String content;
-    private Date createdAt = new Date();
 }
