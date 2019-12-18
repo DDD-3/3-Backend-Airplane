8f31ecc0e075836b0345cd8d3246922500402e0d
윤준석 <y2o2u2n@gmail.com>
2019-12-18 22:15:58 +0900

    테스트 버그 픽스 & TODO 업데이트

--- src/main/java/com/ddd/airplane/chat/room/RoomApiController.java
+++ src/main/java/com/ddd/airplane/chat/room/RoomApiController.java
@@ -2,6 +2,7 @@ package com.ddd.airplane.chat.room;
 
 import com.ddd.airplane.account.Account;
 import com.ddd.airplane.account.CurrentAccount;
+import com.ddd.airplane.chat.message.Message;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -39,6 +40,17 @@ public class RoomApiController {
     @GetMapping("/v1/rooms")
     @ResponseStatus(HttpStatus.OK)
     public List<Room> getRooms(
+            Pageable pageable,
+            @CurrentAccount Account account
+    ) {
+        return List.of();
+    }
+
+    // TODO : 특정 채팅방 내 메세지 조회
+    @GetMapping("/v1/rooms/{roomId}/messages")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Message> getMessages(
+            @PathVariable String roomId,
             Pageable pageable,
             @CurrentAccount Account account
     ) {
--- src/test/java/com/ddd/airplane/common/TestEmbeddedRedisServerConfig.java
+++ src/test/java/com/ddd/airplane/common/TestEmbeddedRedisServerConfig.java
@@ -3,27 +3,23 @@ package com.ddd.airplane.common;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.StringUtils;
 import redis.embedded.RedisServer;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.List;
 
 @Slf4j
 @Configuration
 public class TestEmbeddedRedisServerConfig {
+    private static RedisServer redisServer;
+
     @Value("${spring.redis.port}")
     private Integer redisPort;
 
-    private RedisServer redisServer;
-
     @PostConstruct
-    public void start() throws IOException {
-        if (!isRedisRunning(redisPort)) {
+    public void start() {
+        if (redisServer == null) {
             redisServer = new RedisServer(redisPort);
             redisServer.start();
             log.info("EMBEDDED REDIS START : port={}", redisPort);
@@ -37,44 +33,5 @@ public class TestEmbeddedRedisServerConfig {
             redisServer.stop();
             log.info("EMBEDDED REDIS STOP : port={}", ports);
         }
-    }
-
-    private boolean isRedisRunning(int port) throws IOException {
-        return isRunning(executeGrepProcessCommand(port));
-    }
-
-    private int findAvailablePort() throws IOException {
-        for (int port = 10000; port <= 65535; port++) {
-            Process process = executeGrepProcessCommand(port);
-            if (!isRunning(process)) {
-                return port;
-            }
-        }
-
-        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
-    }
-
-    private Process executeGrepProcessCommand(int port) throws IOException {
-        String command = String.format("netstat -nat | grep LISTEN | grep %d", port);
-        String[] shell = {"/bin/sh", "-c", command};
-        return Runtime.getRuntime().exec(shell);
-    }
-
-    private boolean isRunning(Process process) {
-        String line;
-        StringBuilder pidInfo = new StringBuilder();
-
-        log.info("GREP PROCESS START");
-        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-            while ((line = input.readLine()) != null) {
-                log.info("GREP PROCESS RESULT : {}", line);
-                pidInfo.append(line);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        log.info("GREP PROCESS END");
-
-        return !StringUtils.isEmpty(pidInfo.toString());
     }
 }
